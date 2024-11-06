863c7bed19c9988893592eb593fa253b0e56eeff
Varshith Reddy <varshithreddy355@gmail.com>
2024-11-06 06:28:15 +0530

    init

---
+++ backend/src/routes/blog.ts
@@ -0,0 +1,135 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { createBlogInput, updateBlogInput } from "@varshithreddy355/medium-common";
+import { Hono } from "hono";
+import { use } from "hono/jsx";
+import { verify } from "hono/jwt";
+import { auth } from "hono/utils/basic-auth";
+
+export const blogRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  },
+  Variables: {
+    userId: string;
+  }
+}>();
+
+type User={
+  id:string;
+}
+
+blogRouter.use("/*",async (c,next)=>{
+  const authHeader = c.req.header("authorization") || "";
+  const user = await verify(authHeader,c.env.JWT_SECRET) as User;
+  try{
+    if (user && user.id){
+      c.set("userId", user.id);    
+      await next();
+    }else{
+      c.status(403);
+      return c.json({
+        message : "You have not logged in"
+      })
+    } 
+  }catch(e){
+    c.status(403);
+    return c.json({
+      message : "You have not logged in"
+    })
+  }
+})
+
+blogRouter.post('/', async(c) =>{
+  const body = await c.req.json();
+  const { success } = createBlogInput.safeParse(body);
+  if(!success){
+    c.status(411);
+    return c.json({
+      message : "Invalid inputs"
+    })
+  }
+  const authorId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl:c.env.DATABASE_URL,
+
+  }).$extends(withAccelerate())
+ 
+  const post = await prisma.post.create({
+    data:{
+      title: body.title,
+      content: body.content,
+      thumbnail: body.thumbnail,
+      authorId: authorId
+    }
+  })
+  return c.json({
+    id:post.id
+  })
+})
+ 
+blogRouter.put('/', async(c) =>{
+  const body = await c.req.json();
+  const { success } = updateBlogInput.safeParse(body);
+  if(!success){
+    c.status(411);
+    return c.json({
+      message : "Invalid inputs"
+    })
+  }
+  const prisma = new PrismaClient({
+    datasourceUrl:c.env.DATABASE_URL,
+
+  }).$extends(withAccelerate())
+
+  await prisma.post.update({
+    where:{
+      id: body.id
+    },
+    data:{
+      title: body.title,
+      content: body.content,
+      thumbnail: body.thumbnail
+    }
+  })
+  return c.text('Updated sucessfully');
+})
+
+// Should add the paggination for todos
+blogRouter.get('/bulk', async(c) =>{
+  const prisma = new PrismaClient({
+    datasourceUrl:c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const blogs = await prisma.post.findMany();
+  return c.json({
+    blogs
+  })
+  
+})
+
+blogRouter.get('/:id', async(c) =>{
+  const id = c.req.param("id");
+  const prisma = new PrismaClient({
+    datasourceUrl:c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  try{
+    const post = await prisma.post.findFirst({
+      where:{
+        id: id
+      },
+    })
+    return c.json({
+      post
+    })
+  }catch(e){
+    c.status(411);
+    return c.json({
+      message:"Error while fetching blog"
+    })
+  }
+
+})
+
---
+++ backend/src/routes/user.ts
@@ -0,0 +1,88 @@
+import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { jwt, sign } from 'hono/jwt'
+import { signinInput, signupInput } from "@varshithreddy355/medium-common";
+
+export const userRouter = new Hono<{
+  Bindings:{
+    DATABASE_URL: string,
+    JWT_SECRET: string
+  }
+}>()
+
+userRouter.post('/signup', async (c) => {
+  const body = await c.req.json();
+
+  const { success, error } = signupInput.safeParse(body); 
+  if (!success) {
+    console.log("Zod validation failed:", error); // Log validation errors
+    c.status(411);
+    return c.json({
+      message: "Invalid inputs!!"
+    });
+  }
+
+ 
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email:body.email,
+        password:body.password,
+        name : body.name
+      }
+    });
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e) {
+    console.error("Error:", e);
+    c.status(400);
+    return c.text("Invalid!");
+  }
+});
+
+
+
+userRouter.post('/signin', async (c) => {
+  const body = await c.req.json();
+  console.log("Received Body:", body); // Log the incoming body
+
+  const { success, error } = signinInput.safeParse(body); 
+  if (!success) {
+    console.log("Zod validation failed:", error); // Log validation errors
+    c.status(411);
+    return c.json({
+      message: "Invalid inputs!!",
+      error: error?.format() // Show detailed error
+    });
+  }
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+        password: body.password,
+      }
+    });
+
+    if (!user) {
+      c.status(403);
+      return c.json({ error: "Incorrect credentials" });
+    }
+
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e) {
+    c.status(411);
+    return c.text("Invalid");
+  }
+});
